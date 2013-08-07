f1221aa5f2f159c24bae116b3391f66ececd25c6
hannes <hannes.rauhe@sap.com>
2013-08-07 16:44:32 +0200

    added benchmark... sqlscript

--- kmeans.sql
+++ kmeans.sql
@@ -1,2 +1,12 @@
+select * from TABLE(KMEANSCOLORUDF(2,'CPU'));
+select * from TABLE(KMEANSCOLORUDF(2,'GPU'));
 
-select * from TABLE(KMEANSCOLORUDF(8));
+select * from TABLE(KMEANSCOLORUDF(4,'GPU'));
+select * from TABLE(KMEANSCOLORUDF(4,'CPU'));
+
+select * from TABLE(KMEANSCOLORUDF(8,'CPU'));
+select * from TABLE(KMEANSCOLORUDF(8,'GPU'));
+
+select * from TABLE(KMEANSCOLORUDF(16,'GPU'));
+select * from TABLE(KMEANSCOLORUDF(16,'CPU'));
+
