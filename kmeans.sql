74a5386b6cbdedcb3fa2a4b0c288cc0f6ce8b013
hannes <hannes.rauhe@sap.com>
2013-08-09 17:32:36 +0200

    first working version

--- kmeans.sql
+++ kmeans.sql
@@ -1,12 +1,29 @@
 select * from TABLE(KMEANSCOLORUDF(2,'CPU'));
 select * from TABLE(KMEANSCOLORUDF(2,'GPU'));
 
-select * from TABLE(KMEANSCOLORUDF(4,'GPU'));
+--#BGBLK 3
+select * from TABLE(KMEANSCOLORUDF(2,'CPU'));
+--#EOBLK
+--#BGBLK 3
 select * from TABLE(KMEANSCOLORUDF(4,'CPU'));
-
+--#EOBLK
+--#BGBLK 3
 select * from TABLE(KMEANSCOLORUDF(8,'CPU'));
-select * from TABLE(KMEANSCOLORUDF(8,'GPU'));
-
-select * from TABLE(KMEANSCOLORUDF(16,'GPU'));
+--#EOBLK
+--#BGBLK 3
 select * from TABLE(KMEANSCOLORUDF(16,'CPU'));
+--#EOBLK
+
+--#BGBLK 3
+select * from TABLE(KMEANSCOLORUDF(2,'GPU'));
+--#EOBLK
+--#BGBLK 3
+select * from TABLE(KMEANSCOLORUDF(4,'GPU'));
+--#EOBLK
+--#BGBLK 3
+select * from TABLE(KMEANSCOLORUDF(8,'GPU'));
+--#EOBLK
+--#BGBLK 3
+select * from TABLE(KMEANSCOLORUDF(16,'GPU'));
+--#EOBLK
 
